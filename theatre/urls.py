ddabd47f4e889063565a027ee0f825335788e474
Nataliia Podoroha <ingenuous999@gmail.com>
2023-09-19 11:19:26 +0200

    Add urls.py to apps theatre_service and user and include it to project urls

--- theatre/urls.py
+++ theatre/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/theatre_service/", include("theatre_service.urls", namespace="theatre_service")),
+    path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]

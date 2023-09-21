3f7cea7d154a8ad8c474f947c90a5eaf62610451
Nataliia Podoroha <ingenuous999@gmail.com>
2023-09-21 21:08:30 +0200

    Add paths for ActorViewSet, GenreViewSet, PerformanceViewSet, PlayViewSet, TheatreHallViewSet, ReservationViewSet

--- theatre_service/urls.py
+++ theatre_service/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from theatre_service.views import (
+    ActorViewSet,
+    GenreViewSet,
+    PerformanceViewSet,
+    PlayViewSet,
+    TheatreHallViewSet,
+    ReservationViewSet,
+)
+
 router = routers.DefaultRouter()
+router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
+router.register("performances", PerformanceViewSet)
+router.register("plays", PlayViewSet)
+router.register("reservations", ReservationViewSet)
+router.register("theatre_halls", TheatreHallViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
